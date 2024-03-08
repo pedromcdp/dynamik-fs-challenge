0890e56aeb809f23e2303e0fada84045004e3cfa
Pedro Miguel Pereira <pedromcdp@gmail.com>
2024-03-08 21:59:10 +0000

    refactor(types): added IGetDevByTermResponse, Developer & IGetDevResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,29 @@ export interface IDeveloper extends Document {
   birth_date: Date | string;
   stack: string[] | null;
 }
+
+export interface IGetDevByTermResponse {
+  success: boolean;
+  numberOfResults: number;
+  totalResults: number;
+  page: number;
+  totalPages: number;
+  data: Developer[];
+}
+
+export interface Developer {
+  id: string;
+  nickname: string;
+  name: string;
+  birth_date: string;
+  stack: string[] | null;
+}
+
+export interface IGetDevResponse {
+  sucess: boolean;
+  id: string;
+  nickname: string;
+  name: string;
+  birth_date: string;
+  stack: string[] | null;
+}
