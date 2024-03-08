7ce1c9223a8f788aab9e13c63138b246ee8092a3
Pedro Miguel Pereira <pedromcdp@gmail.com>
2024-03-08 15:03:13 +0000

    feat(appconfig.ts): added App Config file with app name and description

---
+++ src/utils/appConfig.ts
@@ -0,0 +1,9 @@
+interface IAppConfig {
+  name: string;
+  description: string;
+}
+
+export const AppConfig: IAppConfig = {
+  name: "Desafio Dynamik FullStack",
+  description: "Desafio para a posição de FullStack developer na Dynamik.",
+};
