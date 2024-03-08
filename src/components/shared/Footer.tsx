cbfed7b65861a2682f3bb0349824f2511c80d88e
Pedro Miguel Pereira <pedromcdp@gmail.com>
2024-03-08 22:00:40 +0000

    feat(footer): added Footer component to app

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = (): JSX.Element => {
+  return (
+    <footer className="w-full text-center my-4">
+      <span>Pedro Miguel Pereira &copy; {new Date().getFullYear()}</span>
+    </footer>
+  );
+};
+
+export default Footer;
