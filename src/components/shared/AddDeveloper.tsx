1a741882e0dc94d0c107a3571d81549a3860c71f
Pedro Miguel Pereira <pedromcdp@gmail.com>
2024-03-09 19:08:39 +0000

    feat(adddeveloper.tsx): added submit logic to addDeveloper form component

--- src/components/shared/AddDeveloper.tsx
+++ src/components/shared/AddDeveloper.tsx
@@ -1,26 +1,54 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { programmingLanguages } from "@/utils/constants";
 import {
   Modal,
   ModalContent,
   ModalHeader,
   ModalBody,
-  ModalFooter,
   Button,
-  useDisclosure,
   Select,
   SelectItem,
   Input,
+  useDisclosure,
+  ModalFooter,
 } from "@nextui-org/react";
 import { Plus } from "lucide-react";
+import { formSchema } from "@/lib/formSchema";
+import { IAddDeveloper } from "@/types";
+import { postDev } from "@/app/(main)/(routes)/(action)/postDev";
+import { useAction } from "next-safe-action/hooks";
 
 export const AddDeveloper = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IAddDeveloper>({
+    resolver: zodResolver(formSchema),
+  });
+  const { execute, status } = useAction(postDev, {
+    onSuccess: (data) => {
+      if (data.failure) {
+        setError("nickname", {
+          type: "manual",
+          message: data.failure,
+        });
+      }
+    },
+  });
+
+  const onSubmit = handleSubmit((data: IAddDeveloper) => {
+    execute(data);
+  });
 
   return (
     <>
-      <Button variant="light" color="default" onPress={onOpen}>
+      <Button variant="light" color="default" onClick={onOpen}>
         <Plus /> Adicionar
       </Button>
       <Modal
@@ -29,62 +57,74 @@ export const AddDeveloper = () => {
         className="text-foreground"
       >
         <ModalContent>
-          {(onClose) => (
-            <form action="">
-              <ModalHeader className="flex flex-col gap-1">
-                Adicionar Developer
-              </ModalHeader>
-              <ModalBody>
-                <Input
-                  isRequired
-                  type="text"
-                  label="Nickname"
-                  placeholder="Introduza o seu nickname"
-                  className="max-md:w-full md:min-w-96 md:grow"
-                />
-                <Input
-                  isRequired
-                  type="text"
-                  label="Nome"
-                  placeholder="Introduza o seu nome"
-                  className=""
-                />
-
-                <Input
-                  isRequired
-                  type="text"
-                  label="Data de Nascimento"
-                  placeholder="Introduza a sua data de nascimento"
-                  className=""
-                />
-
-                <Select
-                  label="Stack"
-                  placeholder="Selecione o seu stack"
-                  selectionMode="multiple"
-                  className=""
-                >
-                  {programmingLanguages.map((language: string) => (
-                    <SelectItem
-                      key={language}
-                      value={language}
-                      className="text-foreground"
-                    >
-                      {language}
-                    </SelectItem>
-                  ))}
-                </Select>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Cancelar
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Submeter
-                </Button>
-              </ModalFooter>
-            </form>
-          )}
+          <form onSubmit={onSubmit}>
+            <ModalHeader className="flex flex-col gap-1">
+              Adicionar Developer
+            </ModalHeader>
+            <ModalBody>
+              <Input
+                isRequired
+                type="text"
+                label="Nickname"
+                placeholder="Introduza o seu nickname"
+                errorMessage={errors.nickname?.message}
+                {...register("nickname")}
+              />
+              <Input
+                isRequired
+                type="text"
+                label="Nome"
+                placeholder="Introduza o seu nome"
+                errorMessage={errors.name?.message}
+                {...register("name")}
+              />
+              <Input
+                isRequired
+                type="date"
+                max={new Date().toISOString().split("T")[0]}
+                min={"1900-01-01"}
+                label="Data de Nascimento"
+                errorMessage={errors.birthdate?.message}
+                {...register("birthdate")}
+              />
+              <Select
+                label="Stack"
+                placeholder="Selecione o seu stack"
+                errorMessage={errors.stack?.message}
+                {...register("stack")}
+                selectionMode="multiple"
+              >
+                {programmingLanguages.map((language: string) => (
+                  <SelectItem
+                    key={language}
+                    value={language}
+                    className="text-foreground"
+                  >
+                    {language}
+                  </SelectItem>
+                ))}
+              </Select>
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                isDisabled={status === "executing"}
+                color="default"
+                variant="light"
+                onClick={onOpenChange}
+              >
+                Cancelar
+              </Button>
+              <Button
+                isDisabled={status === "executing"}
+                isLoading={status === "executing"}
+                color="primary"
+                variant="solid"
+                type="submit"
+              >
+                Submeter
+              </Button>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </>
