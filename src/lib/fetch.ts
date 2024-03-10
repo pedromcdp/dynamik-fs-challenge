3696057e8e5a249c8361e760263b131308298e50
Pedro Miguel Pereira <pedromcdp@gmail.com>
2024-03-10 23:15:33 +0000

    refactor(fetch): wrapped functions in try catch block

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -2,33 +2,43 @@ import type { IGetDevResponse, IGetDevByTermResponse } from "@/types";
 import { revalidateTag } from "next/cache";
 
 export const getDevById = async (id: string): Promise<IGetDevResponse> => {
-  const response = await fetch(`http://localhost:3000/api/devs/${id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    next: { tags: [id] },
-  });
-  revalidateTag(id);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`http://localhost:3000/api/devs/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      next: { tags: [id] },
+    });
+    revalidateTag(id);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 export const getDevsbyTerm = async (
   term: string,
   page?: number
 ): Promise<IGetDevByTermResponse> => {
-  const response = await fetch(
-    `http://localhost:3000/api/devs?terms=${term}&page=${page}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      next: { tags: [term] },
-    }
-  );
-  revalidateTag(term);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/devs?terms=${term}&page=${page}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        next: { tags: [term] },
+      }
+    );
+    revalidateTag(term);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
