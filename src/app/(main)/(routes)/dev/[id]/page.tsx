03d13577e29f0a28beb6e1265f16e930ba46082c
Pedro Miguel Pereira <pedromcdp@gmail.com>
2024-03-10 18:57:41 +0000

    refactor(/id/page): changed name of component and better validation to the response

--- src/app/(main)/(routes)/dev/[id]/page.tsx
+++ src/app/(main)/(routes)/dev/[id]/page.tsx
@@ -2,14 +2,14 @@ import { TableView } from "@/components/shared/Table";
 import { getDevById } from "@/lib/fetch";
 import { tableHeadData } from "@/utils/constants";
 
-export default async function Page({
+export default async function Detail({
   params,
 }: {
   params: { id: string };
 }): Promise<JSX.Element> {
   const response = await getDevById(params.id);
 
-  return response && response.data.name ? (
+  return response.data && response.data.name ? (
     <TableView headData={tableHeadData} data={[response.data]} />
   ) : (
     <p className="w-full text-center">
